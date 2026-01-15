c633f90326cf5f99f1640c21e6efd3c84602ca81
ldcasilang <dianecasilang19@gmail.com>
2026-01-15 13:53:33 +0800

    fixed /admin routing

--- portfolio_frontend/src/main.tsx
+++ portfolio_frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit'
 import { getFullnodeUrl } from '@mysten/sui/client'
+import { BrowserRouter } from 'react-router-dom' // Keep BrowserRouter
 import App from './App'
 import './index.css'
 
@@ -70,6 +71,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           // Disable auto reconnect
           enableUnsafeBurnerAutoConnect={false}
         >
+          
           <App />
         </WalletProvider>
       </SuiClientProvider>
